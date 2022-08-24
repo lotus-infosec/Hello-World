71172281883de84c6889a585d92dd75af2bbcf5c
Lotus <63892102+lotus-infosec@users.noreply.github.com>
2022-08-24 10:33:41 -0400

    Create hallowerld.py

---
+++ hallowerld.py
@@ -0,0 +1,4 @@
+print("hallowerld")
+# Shoutout to FreeCodeCamp.Org for the print statement!
+print("Welcome to my GitHub!")
+print("This is my first script I'm going to upload here, follow along at rhomaniertaylor.com!")
